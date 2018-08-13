4fb83c458eb82b3f686a9b8384412c740616194d
dsmlai1991 <dsmlai1991@gmail.com>
2018-08-14 00:31:40 +0530

    variables and csv updated

--- Nifty_50.R
+++ Nifty_50.R
@@ -24,19 +24,29 @@ nifty50$Date <- as.Date(nifty50$Date, "%Y-%m-%d")
 #Convert to double using POSIXlt
 nifty50$Date <-as.POSIXct(nifty50$Date,format='%Y-%m-%d')
 
-#For Make below 2 vectors of same length
 today_close <- nifty50$Close
-today_close[length(today_close)] <- 0
-
+today_open <- nifty50$Open
 prev_close <- nifty50$Close[2:length(nifty50$Close)]
+high <- nifty50$High
+low  <- nifty50$Low
+
+today_close[length(today_close)] <- 0
+today_open[length(today_open)] <- 0
+
+#For Make below vectors of same length for doing operations
 prev_close[length(prev_close)+1] <- 0
 
 nifty50$diff_today_prev <- (today_close - prev_close)
 
 #Set last value to 1 becoz we if take log(0/1) : it will be -Inf
 today_close[length(today_close)] <- 1
+today_open[length(today_open)] <- 1
 prev_close[length(prev_close)] <- 1
 nifty50$ln_rate_of_int <- round(log(today_close/prev_close),4)
+nifty50$ln_high_low  <- round(log(high/low),4)
+nifty50$ln_openToday_closePrev  <- round(log(today_open/prev_close),4)
+
+head(nifty50)
 
 #test of Data Normallity
 shapiro.test(nifty50$Open)  #p<0.05 Accept H1: Not Normally Distributed
@@ -45,6 +55,8 @@ shapiro.test(nifty50$Low)   #p<0.05 Accept H1: Not Normally Distributed
 shapiro.test(nifty50$Close) #p<0.05 Accept H1: Not Normally Distributed
 shapiro.test(nifty50$diff_today_prev) #p<0.05 Accept H1: Not Normally Distributed
 shapiro.test(nifty50$ln_rate_of_int) #p>0.05 Accept H0:Normally Distributed
+shapiro.test(nifty50$ln_high_low) #p>0.05 Accept H0:Normally Distributed
+shapiro.test(nifty50$ln_openToday_closePrev) #p>0.05 Accept H0:Normally Distributed
 #test
 
 #updating csv file
